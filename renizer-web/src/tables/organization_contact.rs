9e05b146049ff86b3e0b4a1ac56f932bcea5e997
HasibulHaque21 <148891702+HasibulHaque21@users.noreply.github.com>
2024-04-09 17:37:06 +0600

    implement common apis for OrganizationContact

--- renizer-web/src/tables/organization_contact.rs
+++ renizer-web/src/tables/organization_contact.rs
@@ -6,3 +6,47 @@ pub struct OrganizationContact {
     pub org_id: String,
     pub contact: String,
 }
+
+
+#[cfg(feature = "ssr")]
+pub mod ssr {
+    use super::*;
+    use leptos::leptos_server::ServerFnError;
+    use sqlx::mysql::MySqlQueryResult;
+    use crate::auth::{ssr::pool, get_user};
+
+    pub async fn get_organization_contact(org_id: String) -> Result<OrganizationContact, ServerFnError> {
+        if let Ok(Some(_)) = get_user().await {
+            Ok(sqlx::query_as("SELECT * FROM Organization_Contact_T WHERE org_id = ?")
+                .bind(org_id)
+                .fetch_one(&pool()?)
+                .await?)
+        }else {
+            Err(ServerFnError::ServerError("User is not logged in!".into()))
+        }
+    }  
+
+    pub async fn get_all_organization_contact() -> Result<Vec<OrganizationContact>, ServerFnError> {
+        if let Ok(Some(_)) = get_user().await {
+            Ok(sqlx::query_as("SELECT * FROM Organization_Contact_T")
+                .fetch_all(&pool()?)
+                .await?)
+        }else {
+            Err(ServerFnError::ServerError("User is not logged in!".into()))
+        }
+    }  
+
+    pub async fn insert_organization_contact(organization_contact: OrganizationContact) -> Result<MySqlQueryResult, ServerFnError> {
+        if let Ok(Some(_)) = get_user().await {
+            Ok(sqlx::query("INSERT INTO Organization_Contact_T (org_id, contact) VALUES (?, ?)")
+                .bind(organization_contact.org_id)
+                .bind(organization_contact.contact)
+
+
+                .execute(&pool()?)
+                .await?)
+        }else {
+            Err(ServerFnError::ServerError("User is not logged in!".into()))
+        }
+    }  
+}
