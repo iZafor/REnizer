4359b76adf567e73d17f5e369826bf3152be596a
JM Smaron <jmsmaron2003@gmail.com>
2024-04-09 18:12:58 +0600

    implement common apis for ProjectAssociate

--- renizer-web/src/tables/project_associate.rs
+++ renizer-web/src/tables/project_associate.rs
@@ -10,3 +10,48 @@ pub struct ProjectAssociate {
     pub working_experience: Option<u32>,
     pub associate_type: String,
 }
+
+#[cfg(feature = "ssr")]
+pub mod ssr {
+    use super::*;
+    use leptos::leptos_server::ServerFnError;
+    use sqlx::mysql::MySqlQueryResult;
+    use crate::auth::{ssr::pool, get_user};
+
+    pub async fn get_project_associate(p_user_id: String) -> Result<ProjectAssociate, ServerFnError> {
+        if let Ok(Some(_)) = get_user().await {
+            Ok(sqlx::query_as("SELECT * FROM Project_Associate_T WHERE p_user_id = ?")
+                .bind(p_user_id)
+                .fetch_one(&pool()?)
+                .await?)
+        }else {
+            Err(ServerFnError::ServerError("User is not logged in!".into()))
+        }
+    }  
+
+    pub async fn get_all_project_associates() -> Result<Vec<ProjectAssociate>, ServerFnError> {
+        if let Ok(Some(_)) = get_user().await {
+            Ok(sqlx::query_as("SELECT * FROM  Project_Associate_T")
+                .fetch_all(&pool()?)
+                .await?)
+        }else {
+            Err(ServerFnError::ServerError("User is not logged in!".into()))
+        }
+    }  
+
+    pub async fn insert_project_associate(project_associate: ProjectAssociate) -> Result<MySqlQueryResult, ServerFnError> {
+        if let Ok(Some(_)) = get_user().await {
+            Ok(sqlx::query("INSERT INTO Project_Associate_T (p_user_id, hourly_rate, working_experience, associate_type) VALUES (?, ?, ?, ?)")
+                .bind(project_associate.p_user_id)
+                .bind(project_associate.hourly_rate)
+                .bind(project_associate.working_experience)
+                .bind(project_associate.associate_type)
+
+
+                .execute(&pool()?)
+                .await?)
+        }else {
+            Err(ServerFnError::ServerError("User is not logged in!".into()))
+        }
+    }  
+}
