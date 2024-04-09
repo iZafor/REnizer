c3bdd8783d294fb6a0dda38eb4c7e30f0942e1a1
HasibulHaque21 <148891702+HasibulHaque21@users.noreply.github.com>
2024-04-09 17:33:54 +0600

    implement common apis for Investor

--- renizer-web/src/tables/investor.rs
+++ renizer-web/src/tables/investor.rs
@@ -8,3 +8,47 @@ pub struct Investor {
     pub i_user_id: String,
     pub investor_type: String,
 }
+
+
+#[cfg(feature = "ssr")]
+pub mod ssr {
+    use super::*;
+    use leptos::leptos_server::ServerFnError;
+    use sqlx::mysql::MySqlQueryResult;
+    use crate::auth::{ssr::pool, get_user};
+
+    pub async fn get_investor(i_user_id: String) -> Result<Investor, ServerFnError> {
+        if let Ok(Some(_)) = get_user().await {
+            Ok(sqlx::query_as("SELECT * FROM Investor_T WHERE i_user_id = ?")
+                .bind(i_user_id)
+                .fetch_one(&pool()?)
+                .await?)
+        }else {
+            Err(ServerFnError::ServerError("User is not logged in!".into()))
+        }
+    }  
+
+    pub async fn get_all_investors() -> Result<Vec<Investor>, ServerFnError> {
+        if let Ok(Some(_)) = get_user().await {
+            Ok(sqlx::query_as("SELECT * FROM Investor_T")
+                .fetch_all(&pool()?)
+                .await?)
+        }else {
+            Err(ServerFnError::ServerError("User is not logged in!".into()))
+        }
+    }  
+
+    pub async fn insert_investor(investor: Investor) -> Result<MySqlQueryResult, ServerFnError> {
+        if let Ok(Some(_)) = get_user().await {
+            Ok(sqlx::query("INSERT INTO Investor_T (i_user_id, investor_type) VALUES (?, ?)")
+                .bind(investor.i_user_id)
+                .bind(investor.investor_type)
+
+
+                .execute(&pool()?)
+                .await?)
+        }else {
+            Err(ServerFnError::ServerError("User is not logged in!".into()))
+        }
+    }  
+}
